ef647a6b5dd679a03cf706dfb6765020e23c7889
TProgramer <tposter@naver.com>
2023-11-08 01:44:44 +0000

    :ambulance: Hotfix: modify cp command to exclude .github folder in build.sh

--- build.sh
+++ build.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 cd ../
 mkdir output
-cp -R ./Algorithm-Roadmap-Site-FE/* ./output
+# cp -R ./Algorithm-Roadmap-Site-FE/* ./output
+\cp -Rf $(ls -a ./Algorithm-Roadmap-Site-FE/ | grep -Ev "^.github$|^.$|^..$") ../output
 cp -R ./output ./Algorithm-Roadmap-Site-FE/
