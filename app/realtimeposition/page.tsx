cc2ee071a327cdff92274e83fc5050e8f1fdf862
TProgramer <tposter@naver.com>
2023-11-18 14:21:20 +0000

    :recycle: Fix: 이전 생성된 마커가 계속 남아있는 에러 수정

--- app/realtimeposition/page.tsx
+++ app/realtimeposition/page.tsx
@@ -12,6 +12,7 @@ declare global {
 }
 
 const WebSocketPage = () => {
+
   const stompClient = useRef<Client | null>(null);
   const [userName, setUserName] = useState("");
   const [channelId, setChannelId] = useState("");
@@ -19,10 +20,11 @@ const WebSocketPage = () => {
   const [targetStationLong, setTargetStationLong] = useState(0);
   const [targetStationLat, setTargetStationLat] = useState(0);
   const [isSharingLocation, setIsSharingLocation] = useState(false);
-  const [markerArray, setMarkerArray] = useState<any[]>([]);
+
   const { myRoomName, roominfo } = useRoomInfoStore();
   const { fixStation } = roominfo;
   const coodRef = useRef({ lat: null, lng: null });
+
 
   const token = Cookies.get("Bearer");
   if (token && userName === "") {
@@ -31,20 +33,27 @@ const WebSocketPage = () => {
     const result = JSON.parse(payload.toString());
     setUserName(result.nickName);
     setChannelId(myRoomName);
+
+    console.log(userName);
+    console.log(channelId);
   }
+
 
   // 출발지 좌표를 가져오기 위한 fetch
   useEffect(() => {
     const fetchStationInfo = async () => {
       try {
         const encodedFixStation = encodeURIComponent(fixStation);
-        const response = await fetch(
-          `${process.env.NEXT_PUBLIC_serverURL}/map/getStationInfo?fixStation=${encodedFixStation}`,
-        );
+        const response = await fetch(`${process.env.NEXT_PUBLIC_serverURL}/map/getStationInfo?fixStation=${encodedFixStation}`);
         const data = await response.json();
         // 가져온 데이터를 사용할 수 있습니다.
+        console.log('여기여기여기여기:', data);
+
+        console.log('경도 :', data[0].longitude);
+        console.log('위도 :', data[0].latitude);
         setTargetStationLong(data[0].longitude);
         setTargetStationLat(data[0].latitude);
+
       } catch (error) {
         console.error("Error fetching station info:", error);
       }
@@ -53,6 +62,8 @@ const WebSocketPage = () => {
     // 페이지 로딩 시 한 번 실행
     fetchStationInfo();
   }, []);
+
+
 
   interface Station {
     stationId?: number;
@@ -91,30 +102,61 @@ const WebSocketPage = () => {
     stompClient.current.onConnect = (frame) => {
       console.log("Stomp Connected:", frame);
 
-      const position: RealTimePosition[] = [];
+      const positionsArray: RealTimePosition[] = [];
 
       // Stomp 서버에서 메시지를 받는 로직
       stompClient.current?.subscribe(`/sub/channel/${channelId}`, (message) => {
-        position.push(JSON.parse(message.body));
-        setStartStation(JSON.parse(message.body));
-        setPositions([...position]);
+        console.log(JSON.parse(message.body));
+        // positionsArray.push(JSON.parse(message.body));
+        // setStartStation(JSON.parse(message.body));
+        // setPositions([...position]);
 
         const newLocation = JSON.parse(message.body);
+
+        const existingPositionIndex = positionsArray.findIndex(pos => pos.sender === newLocation.sender);
+
+        if (existingPositionIndex !== -1) {
+        // 중복된 sender가 있을 경우, 가장 최신의 위치 정보로 갱신
+        positionsArray[existingPositionIndex] = newLocation;
+
+        // 지도에 있는 해당 마커를 업데이트
+        setPoints((prevPoints) => {
+          const updatedPoints = prevPoints.map(point => {
+            if (point.title === newLocation.sender) {
+              return {
+                title: newLocation.sender,
+                latlng: new window.kakao.maps.LatLng(newLocation.data.latitude, newLocation.data.longitude),
+              };
+            }
+            return point;
+          });
+
+          return updatedPoints;
+        });
+      } else {
+        // 중복된 sender가 없을 경우, 새로운 위치 정보를 추가
+        positionsArray.push(newLocation);
+
+        // 지도에 마커 추가
         setPoints((prevPoints) => [
           ...prevPoints,
           {
             title: newLocation.sender,
-            latlng: new window.kakao.maps.LatLng(
-              newLocation.data.latitude,
-              newLocation.data.longitude,
-            ),
+            latlng: new window.kakao.maps.LatLng(newLocation.data.latitude, newLocation.data.longitude),
           },
         ]);
+      }
 
-        // 기존 위치 정보 업데이트
-        setStartStation(newLocation);
-        setPositions((prevPositions) => [...prevPositions, newLocation]);
+      // 상태 갱신
+      setPositions([...positionsArray]);
+
+      // 기존 위치 정보 업데이트
+      setStartStation(newLocation);
+      setPositions((prevPositions) => [...prevPositions, newLocation]);
+
       });
+      console.log([...positionsArray]);
+      console.log(positionsArray);
     };
   };
 
@@ -128,6 +170,9 @@ const WebSocketPage = () => {
           channelId: channelId,
           data: { latitude, longitude },
         };
+        console.log('서버 보내기 전 체크' + userName)
+
+        console.log(latitude, longitude);
 
         // Stomp 서버로 메시지 보내기
         stompClient.current?.publish({
@@ -142,6 +187,7 @@ const WebSocketPage = () => {
     );
   };
 
+
   useEffect(() => {
     connectAndSubscribe();
     return () => {
@@ -155,26 +201,28 @@ const WebSocketPage = () => {
   const mapRef = useRef<any>(null);
   let map: undefined;
 
-  const [points, setPoints] = useState<{ title: string; latlng: any }[]>([]);
+
+  const [points, setPoints] = useState<{ title: string; latlng: any; }[]>([]);
 
   useEffect(() => {
     const fetchData = async () => {
-      const kakaoMapScript = document.createElement("script");
+
+
+      const kakaoMapScript = document.createElement('script');
       kakaoMapScript.async = true;
       kakaoMapScript.src = `//dapi.kakao.com/v2/maps/sdk.js?appkey=${process.env.NEXT_PUBLIC_kakaoMapKey}&autoload=false`;
       document.head.appendChild(kakaoMapScript);
-      ///////////////////////////////////////////
+      /////////////////////////////////////////// 
 
       const onLoadKakaoAPI = () => {
         window.kakao.maps.load(() => {
-          var container = document.getElementById("map");
+          var container = document.getElementById('map');
+          console.log(coodRef);
           if (container) {
             navigator.geolocation.getCurrentPosition((point) => {
               var options = {
-                center: new window.kakao.maps.LatLng(
-                  point.coords.latitude,
-                  point.coords.longitude,
-                ),
+
+                center: new window.kakao.maps.LatLng(point.coords.latitude, point.coords.longitude),
 
                 level: 3,
               };
@@ -185,48 +233,32 @@ const WebSocketPage = () => {
               if (container) {
                 container.style.marginTop = "-80px";
               }
-              var centerMarkerPosition = new window.kakao.maps.LatLng(
-                targetStationLat,
-                targetStationLong,
-              );
+              var centerMarkerPosition = new window.kakao.maps.LatLng(targetStationLat, targetStationLong);
 
               var centermarker = new window.kakao.maps.Marker({
                 position: centerMarkerPosition,
-                title: "도착지",
+                title: '도착지'
               });
 
               // 마커가 지도 위에 표시되도록 설정합니다
               centermarker.setMap(map);
 
-              var imageSrc =
-                "https://t1.daumcdn.net/localimg/localimages/07/mapapidoc/markerStar.png";
+              var imageSrc = "https://t1.daumcdn.net/localimg/localimages/07/mapapidoc/markerStar.png";
 
               var bounds = new kakao.maps.LatLngBounds();
 
               // centermarker의 위치를 bounds에 추가
               bounds.extend(centerMarkerPosition);
 
-              // useState markerArray가 있으면 기존 마커 제거하기
-              if (markerArray.length != 0) {
-                for (var i = 0; i < markerArray.length; i++) {
-                  markerArray[i].setMap(null);
-                }
-              }
-
-              const tempArr: any[] = [];
-
               for (var i = 0; i < points.length; i++) {
                 var imageSize = new kakao.maps.Size(24, 35);
-                var markerImage = new kakao.maps.MarkerImage(
-                  imageSrc,
-                  imageSize,
-                );
+                var markerImage = new kakao.maps.MarkerImage(imageSrc, imageSize);
 
                 var marker = new kakao.maps.Marker({
                   map: map,
                   position: points[i].latlng,
                   title: points[i].title,
-                  image: markerImage,
+                  image: markerImage, 
                 });
 
                 // 각 마커의 위치를 bounds에 추가
@@ -235,26 +267,23 @@ const WebSocketPage = () => {
                 // 각 마커에 텍스트를 표시하는 CustomOverlay 생성
                 var customOverlay = new window.kakao.maps.CustomOverlay({
                   position: points[i].latlng,
-                  content:
-                    '<div style="point: absolute; text-align: center; white-space: nowrap; top: 5px;">' +
-                    '<div style="display: inline-block; background-color: white; padding: 5px; border-radius: 5px; box-shadow: 1px 1px 5px rgba(0, 0, 0, 0.3); font-size:12px">' +
-                    points[i].title +
-                    "</div>" +
-                    "</div>",
+                  yAnchor: 0,
+                  zIndex: 1,
+                  content: '<div style="point: absolute; text-align: center; white-space: nowrap; top: 5px;">' +
+                    '<div style="display: inline-block; background-color: white; padding: 5px; border-radius: 5px; box-shadow: 1px 1px 1px rgba(0, 0, 0, 0.1); font-size:12px;">' +
+                    points[i].title + 
+                    '</div>' +
+                    '</div>',
                 });
 
                 // CustomOverlay 지도에 표시
                 customOverlay.setMap(map);
-                // customOverlay 배열에 더하기
-                tempArr.push(customOverlay);
               }
-
-              // 마커 배열을 useState에 넣기
-              setMarkerArray(tempArr);
 
               // 현재 좌표와 일정 범위를 고려하여 bounds를 조절
               var extraPadding = 0.5; // 상하좌우 여백 비율 조절 가능
               map.setBounds(bounds, extraPadding);
+
             });
           } else {
             console.error("Container element not found");
@@ -262,67 +291,60 @@ const WebSocketPage = () => {
         });
       };
 
-      kakaoMapScript.addEventListener("load", onLoadKakaoAPI);
-    };
+      kakaoMapScript.addEventListener('load', onLoadKakaoAPI);
+    }
     fetchData();
-    return () => {};
+    return () => {
+    };
+
   }, [points, targetStationLong, targetStationLat]); // points 의존성으로 추가
 
   const handleClick = () => {
-    navigator.geolocation.getCurrentPosition(
-      (point) => {
-        const newLocation = {
-          // title: '내 위치',
-          title: userName,
-          latlng: new window.kakao.maps.LatLng(
-            point.coords.latitude,
-            point.coords.longitude,
-          ),
-        };
 
-        setPoints((prevPoints) => [...prevPoints, newLocation]);
-      },
-      (error) => {
-        console.error("Error getting current point:", error);
-      },
-    );
+    navigator.geolocation.getCurrentPosition((point) => {
+      const newLocation = {
+        // title: '내 위치',
+        title: userName,
+        latlng: new window.kakao.maps.LatLng(point.coords.latitude, point.coords.longitude),
+      };
+
+      setPoints((prevPoints) => [
+        ...prevPoints,
+        newLocation,
+      ]);
+
+
+    }, (error) => {
+      console.error("Error getting current point:", error);
+    });
+
+    console.log('New userName:', userName);
   };
 
   const testFunction = () => {
     shareLocation();
     handleClick();
-  };
+  }
 
-  //5초에 한번씩
+  //5초에 한번씩 
   useEffect(() => {
     let timer = setInterval(() => {
       shareLocation();
+      console.log(positions);
     }, 5000);
 
     return () => clearInterval(timer);
   }, []);
 
+
   return (
-    <main className="flex w-full flex-col items-center justify-center pt-4">
-      <div
-        className="h-[50vh] w-full sm:h-[60vh] md:h-[70vh] lg:h-[80vh]"
-        style={{ position: "relative" }}
-      >
+    <main className="w-full flex flex-col items-center justify-center pt-4">
+      <div className="w-full h-[50vh] sm:h-[60vh] md:h-[70vh] lg:h-[80vh]" style={{ position: "relative" }}>
         <div id="map" style={{ width: "100%", height: "100%" }}></div>
-        <button
-          className="w-30 mt-4 flex items-center justify-center rounded-md border border-gray-300 px-3 py-2 text-center text-blue-800 transition-all duration-75 hover:border-gray-800 focus:outline-none active:bg-gray-100"
-          onClick={testFunction}
-          style={{
-            position: "absolute",
-            bottom: "5px",
-            left: "50%",
-            transform: "translateX(-50%)",
-          }}
-        >
-          내 위치 공유하기
-        </button>
+        <button className="w-30 mt-4 flex items-center justify-center rounded-md border border-gray-300 px-3 py-2 text-center text-blue-800 transition-all duration-75 hover:border-gray-800 focus:outline-none active:bg-gray-100" onClick={testFunction} style={{ position: "absolute", bottom: "5px", left: "50%", transform: "translateX(-50%)" }}>내 위치 공유하기</button>
 
         {/* {isSharingLocation && <p className="items-center text-center"> {userName} 님이 실시간 위치를 공유하고 있어요</p>} */}
+
       </div>
     </main>
   );
